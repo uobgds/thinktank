e2222c6877b655777b6937465cc4588ef5c868bf
sxg661 <sxg661@student.bham.ac.uk>
2018-10-29 18:02:18 +0000

    input manager is now more abstract so we can use it for more types of input

---
+++ Assets/Scripts/Input/InputModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputModule {
+
+    protected readonly float MAX_SPEED = 10f;
+
+    public Vector2 position;
+
+    public int shapeNum;
+
+    public abstract void updateInput();
+
+    private void Start()
+    {
+        position = new Vector2(0, 0);
+        shapeNum = 1;
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Input/KeyboardInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : InputModule {
+
+
+    public override void updateInput()
+    {
+        //Deals with the position of the robot
+        //Input from arrow keys
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        float currentXVelocity = moveHorizontal * MAX_SPEED;
+        float currentYVelocity = moveVertical * MAX_SPEED;
+
+        position[0] = position[0] + currentXVelocity;
+        position[1] = position[1] + currentYVelocity;
+
+        //Deals with toggling between shapes
+        if (Input.GetKeyUp("space"))
+        {
+            shapeNum = ++shapeNum % InputManager.NUMBEROFSHAPES;
+
+        }
+    }
+
+}
