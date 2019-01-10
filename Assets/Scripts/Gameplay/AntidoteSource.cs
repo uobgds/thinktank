8f3b888fc9780474ab36b6cd261ae8e008b36c2c
JamesAdey <jamesadey91@gmail.com>
2019-01-10 19:58:36 +0000

    partial antidote system

---
+++ Assets/Scripts/Gameplay/AntidoteSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntidoteSource : MonoBehaviour {
+
+    public float rate = 1.618f;
+
+    void OnTriggerStay(Collider other)
+    {
+        PlayerController pl = other.GetComponent<PlayerController>();
+
+        if (pl == null)
+        {
+            return;
+        }
+
+        pl.FillAntidote(rate);
+    }
+}
