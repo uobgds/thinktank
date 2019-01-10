d774d96f27751c15a81409b39893a64172b29278
sxg661 <sxg661@student.bham.ac.uk>
2019-01-10 20:14:53 +0000

    made a drainer class to go on the inner walls

---
+++ Assets/Scripts/Gameplay/AntidoteDrainer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntidoteDrainer : MonoBehaviour
+{
+
+
+    private float drainageRate = -0.5f;
+
+    void OnTriggerStay(Collider other)
+    {
+        PlayerController pl = other.GetComponent<PlayerController>();
+
+        if (pl == null)
+        {
+            return;
+        }
+
+        pl.FillAntidote(drainageRate);
+        
+    }
+}
